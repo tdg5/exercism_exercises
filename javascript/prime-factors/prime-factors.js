9c0452397bd33aef293e98d562d9a62eda7069d0
Danny Guinther <dannyguinther@gmail.com>
2015-12-11 07:25:15 -0500

    Tweak prime-factors based on feedback

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -6,11 +6,10 @@ class PrimeFactors {
     for(let i = 2; i <= Math.sqrt(composite); ++i) {
       while (composite % i === 0) {
         factors.push(i);
-        composite = composite / i;
+        composite /= i;
       }
     }
     if (composite > 1) { factors.push(composite); }
-    factors.sort(function(a,b) { return a < b ? -1 : a > b ? 1 : 0; });
     return factors;
   }
 }
